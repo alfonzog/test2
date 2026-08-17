1e20713551034f2214e7395c9e835cf23b3abe33
José Armando Alfonzo García <alfonzog@hotmail.com>
2026-08-16 20:16:52 -0600

    Mejoras 3

--- test2/test2.cpp
+++ test2/test2.cpp
@@ -7,6 +7,7 @@ int main()
 {
     std::cout << "Hello World!\n";
     std::cout << "Mejoras\n";
+    std::cout << "Mas cambios\n";
 }
 
 // Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
